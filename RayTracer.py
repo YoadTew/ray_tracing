d2d919c6c5462bce5a9ba812ca4db554e6b68b1b
YoadTew <yoad.tewel@gmail.com>
2021-04-10 01:25:17 +0300

    Started calculating rays

--- RayTracer.py
+++ RayTracer.py
@@ -1,4 +1,22 @@
 from modules.Scene import Scene
+import argparse
+import os
+import time
+import numpy as np
+from PIL import Image
+
+def read_args():
+    parser = argparse.ArgumentParser(description='Ray tracer running script')
+
+    parser.add_argument('scene_path', help='path to scene file')
+    parser.add_argument('img_path', help='path to save image')
+
+    parser.add_argument('img_width', default=500, nargs='?', type=float, help='Image width (default: 500)')
+    parser.add_argument('img_height', default=500, nargs='?', type=float, help='Image height (default: 500)')
+
+    args = parser.parse_args()
+
+    return args
 
 def read_scene(scene_path):
     with open(scene_path, 'r') as scene_file:
@@ -8,6 +26,47 @@ def read_scene(scene_path):
 
         return scene
 
-scene = read_scene('scenes/Pool.txt')
+def save_img(img, img_path):
+    PIL_img = Image.fromarray(img)
+    PIL_img.save(img_path)
 
-print('Done!')
+def calc_screen_vectors(scene):
+    Vz = scene.camera.look_at - scene.camera.position
+    Vz = Vz / np.linalg.norm(Vz)
+    a, b, c = Vz
+
+    Sx = -b
+    Cx = np.sqrt(1 - Sx ** 2)
+    Sy = -a / Cx
+    Cy = c / Cx
+
+    M = np.matrix([[Cy, 0, Sy], [-Sx * Sy, Cx, Sx * Cy], [-Cx * Sy, -Sx, Cx * Cy]])
+    Vx = np.matmul(M, [1, 0, 0])
+    Vy = np.matmul(M, [0, 1, 0])
+
+    return Vx, Vy, Vz
+
+def main():
+    args = read_args()
+    scene = read_scene(args.scene_path)
+
+    img = np.zeros((args.img_height, args.img_width), dtype=np.uint8)
+
+    # Calculate normalize screen vectors
+    Vx, Vy, Vz = calc_screen_vectors(scene)
+
+    # Calc screen values
+    s_center = scene.camera.position + Vz * scene.camera.screen_distance
+    s_width = scene.camera.screen_width
+    s_height = (args.img_height / args.img_width) * s_width
+
+    P_0 = s_center - (s_width / 2) * Vx - (s_height / 2) * Vy
+
+    for index_y in range(args.img_height):
+        for index_x in range(args.img_width):
+            img[index_y, index_x] = (index_x + index_y)
+
+    save_img(img, args.img_path)
+
+if __name__ == '__main__':
+    main()
